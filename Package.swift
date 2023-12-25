71a87ef94910463646fe3e4de9f754fd54de5585
nixberg <310825+nixberg@users.noreply.github.com>
2023-12-25 17:54:12 +0100

    Delete AsyncSink.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "sink-swift",
-    platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15),
-    ],
     products: [
         .library(
             name: "Sink",
--- Sources/Sink/AsyncSink.swift
+++
@@ -1,7 +0,0 @@
-public protocol AsyncSink<Element>: Sink {
-    associatedtype Element
-    
-    mutating func append(_ element: Element) async throws
-    
-    mutating func append(contentsOf sequence: some Sequence<Element>) async throws
-}
