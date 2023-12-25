4ca5c94c3db9434f515b02f5b7a01858a81c8b87
nixberg <310825+nixberg@users.noreply.github.com>
2023-12-25 17:55:29 +0100

    Update SinkAdapter.swift

--- Sources/Sink/SinkAdapter.swift
+++ Sources/Sink/SinkAdapter.swift
@@ -1,33 +1,38 @@
-public struct SinkAdapter<Base: MutableCollection>: Sink {
-    public typealias Element = Base.Element
-    private let basePointer: UnsafeMutablePointer<Base>
-    
-    private var index: Base.Index
-    
-    init(basePointer: UnsafeMutablePointer<Base>) {
-        self.basePointer = basePointer
-        index = basePointer.pointee.startIndex
-    }
-    
-    public mutating func append(_ element: Element) {
-        basePointer.pointee[index] = element
-        index = basePointer.pointee.index(after: index)
-    }
-    
-    public mutating func append(contentsOf sequence: some Sequence<Element>) {
-        for element in sequence {
-            self.append(element)
-        }
-    }
-}
-
 extension MutableCollection {
     public mutating func withSink<R>(
         _ body: (inout SinkAdapter<Self>) throws -> R
     ) rethrows -> R {
         try withUnsafeMutablePointer(to: &self) {
-            var adapter = SinkAdapter(basePointer: $0)
+            var adapter = SinkAdapter(base: $0)
             return try body(&adapter)
         }
     }
+}
+
+public struct SinkAdapter<Base: MutableCollection> {
+    public typealias Element = Base.Element
+    
+    private let base: UnsafeMutablePointer<Base>
+    
+    private var index: Base.Index
+    
+    fileprivate init(base: UnsafeMutablePointer<Base>) {
+        self.base = base
+        index = base.pointee.startIndex
+    }
+}
+
+extension SinkAdapter: Sink {
+    public mutating func append(_ element: Element) {
+        base.pointee[index] = element
+        index = base.pointee.index(after: index)
+    }
+    
+    public mutating func append(contentsOf newElements: some Sequence<Element>) {
+        for newElement in newElements {
+            self.append(newElement)
+        }
+    }
+    
+    public mutating func reserveCapacity(_ minimumCapacity: Int) {}
 }
